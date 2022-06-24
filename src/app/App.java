b487b8a25dda8c19d2c4bee0da945877d729eb9c
ak <abhijit.k939@gmail.com>
2022-06-23 20:46:32 -0400

    Format `App.java` code

--- src/app/App.java
+++ src/app/App.java
@@ -1,4 +1,5 @@
 package app;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -8,275 +9,259 @@ import java.util.Vector;
 
 import java.sql.*;
 
-
 public class App {
 
 	// Get logger for this class
-	private static java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(App.class.getSimpleName());
-	
+	private static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(App.class.getSimpleName());
+
 	private static Scanner in = new Scanner(System.in);
-	
+
 	/**
-     * Connects to the database if it exists, creates it if it does not, and returns the connection object.
-     * 
-     * @param databaseFileName the database file name
-     * @return a connection object to the designated database
-     */
-    public static Connection initializeDB(String databaseFileName) {
-    	/**
-    	 * The "Connection String" or "Connection URL".
-    	 * 
-    	 * "jdbc:sqlite:" is the "subprotocol".
-    	 * (If this were a SQL Server database it would be "jdbc:sqlserver:".)
-    	 */
-        String url = "jdbc:sqlite:" + databaseFileName;
-        Connection conn = null; // If you create this variable inside the Try block it will be out of scope
-        try {
-            conn = DriverManager.getConnection(url);
-            if (conn != null) {
-            	// Provides some positive assurance the connection and/or creation was successful.
-                System.out.println("The connection to the database was successful.");
-            } else {
-            	// Provides some feedback in case the connection failed but did not throw an exception.
-            	System.out.println("Null Connection");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            System.out.println("There was a problem connecting to the database.");
-        }
-        return conn;
-    }
-    
-    // Get names of all tables in the database.
-    public static HashSet<String> getTableNames(Connection conn) {
-    	HashSet<String> tableNames = new HashSet<String>();;
-    	
-    	try {
-	    	DatabaseMetaData meta = conn.getMetaData();
-	    	ResultSet tRes =  meta.getTables(null, null, "%", null);
-	    	
-	    	while (tRes.next()) {
-	    		tableNames.add(tRes.getString("TABLE_NAME"));
-	    	}
-    	}
-    	catch (Exception e) {
-    		System.out.println(e.getMessage());
-    	}
-    	
-    	return tableNames;
-    }
-    
-    // Get names of all columns in a table.
-    public static TreeSet<String> getColumnNames(Connection conn, String tableName) throws Exception { // TODO: Specialize exception thrown
-    	
-    	// Make sure table name provided exists in the database.
-    	if (!getTableNames(conn).contains(tableName)) {
-    		System.out.println("X Given table name does not exist in the database. X"); // TODO: Better error handling.
-    		System.exit(1);
-    	}
-    	
-    	// TODO: Figure out a better way without potentially retrieving the whole table.
-    	ResultSet rst = null;
-    	try {
-	    	String prepQuery = "SELECT *\n" +
-					   		   "FROM ?\n" +
-					   		   "WHERE FALSE;";
-			
-	    	PreparedStatement pStmt=  conn.prepareStatement(prepQuery);
-	    	
-	    	pStmt.setString(1, tableName); // add in the table name
-	    	
-	    	rst = pStmt.executeQuery();
+	 * Connects to the database if it exists, creates it if it does not, and returns
+	 * the connection object.
+	 * 
+	 * @param databaseFileName the database file name
+	 * @return a connection object to the designated database
+	 */
+	public static Connection initializeDB(String databaseFileName) {
+		/**
+		 * The "Connection String" or "Connection URL".
+		 * 
+		 * "jdbc:sqlite:" is the "subprotocol". (If this were a SQL Server database it
+		 * would be "jdbc:sqlserver:".)
+		 */
+		String url = "jdbc:sqlite:" + databaseFileName;
+		Connection conn = null; // If you create this variable inside the Try block it will be out of scope
+		try {
+			conn = DriverManager.getConnection(url);
+			if (conn != null) {
+				// Provides some positive assurance the connection and/or creation was
+				// successful.
+				System.out.println("The connection to the database was successful.");
+			} else {
+				// Provides some feedback in case the connection failed but did not throw an
+				// exception.
+				System.out.println("Null Connection");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println("There was a problem connecting to the database.");
+		}
+		return conn;
+	}
 
-    	}
-    	catch (Exception e) {
-    		System.out.println(e.getMessage());
-    		throw e;
-    	}
+	// Get names of all tables in the database.
+	public static HashSet<String> getTableNames(Connection conn) {
+		HashSet<String> tableNames = new HashSet<String>();
+		;
 
-    	TreeSet<String> colNames = getColumnNames(rst);
-    	return colNames;
-    	
-    }
-    
-    // Get names of all columns in a query result set.
-    public static TreeSet<String> getColumnNames(ResultSet rst) {
-    	TreeSet<String> colNames = new TreeSet<String>();
-    	
-    	// TODO: Figure out a better way without potentially retrieving the whole table.
-    	try {
+		try {
+			DatabaseMetaData meta = conn.getMetaData();
+			ResultSet tRes = meta.getTables(null, null, "%", null);
 
-	    	ResultSetMetaData rstMeta = rst.getMetaData();
-	    	
-	    	int numCols = rstMeta.getColumnCount();
-	    	
-	    	for (int c = 1; c <= numCols; ++c) {
-	    		colNames.add(rstMeta.getColumnName(c));
-	    		rstMeta.getColumnType(c);
-	    	}
-    	}
-    	catch (Exception e) {
-    		System.out.println(e.getMessage());
-    	}
-    	
-    	return colNames;
-    }
-    
-    // Get type of data in a column for a query result set.
-    public static Vector<Integer> getColumnTypes(ResultSet rst) {
-    	Vector<Integer> colTypes = new Vector<Integer>();
-    	
-    	// TODO: Figure out a better way without potentially retrieving the whole table.
-    	try {
+			while (tRes.next()) {
+				tableNames.add(tRes.getString("TABLE_NAME"));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-	    	ResultSetMetaData rstMeta = rst.getMetaData();
-	    	
-	    	int numCols = rstMeta.getColumnCount();
+		return tableNames;
+	}
 
-	    	for (int c = 1; c <= numCols; ++c) {
-	    		int t = rstMeta.getColumnType(c); // TODO: Why does it returns an "int" when doc says "java.sql.Types".
-	    		colTypes.add(t);
-	    	}
-    	}
-    	catch (Exception e) {
-    		System.out.println(e.getMessage());
-    	}
-    	
-    	return colTypes;
-    }
-    
-    // All possible main menu options.
+	// Get names of all columns in a table.
+	public static TreeSet<String> getColumnNames(Connection conn, String tableName) throws Exception { // TODO:
+																										// Specialize
+																										// exception
+																										// thrown
+
+		// Make sure table name provided exists in the database.
+		if (!getTableNames(conn).contains(tableName)) {
+			System.out.println("X Given table name does not exist in the database. X"); // TODO: Better error handling.
+			System.exit(1);
+		}
+
+		// TODO: Figure out a better way without potentially retrieving the whole table.
+		ResultSet rst = null;
+		try {
+			String prepQuery = "SELECT *\n" + "FROM ?\n" + "WHERE FALSE;";
+
+			PreparedStatement pStmt = conn.prepareStatement(prepQuery);
+
+			pStmt.setString(1, tableName); // add in the table name
+
+			rst = pStmt.executeQuery();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+
+		TreeSet<String> colNames = getColumnNames(rst);
+		return colNames;
+
+	}
+
+	// Get names of all columns in a query result set.
+	public static TreeSet<String> getColumnNames(ResultSet rst) {
+		TreeSet<String> colNames = new TreeSet<String>();
+
+		// TODO: Figure out a better way without potentially retrieving the whole table.
+		try {
+
+			ResultSetMetaData rstMeta = rst.getMetaData();
+
+			int numCols = rstMeta.getColumnCount();
+
+			for (int c = 1; c <= numCols; ++c) {
+				colNames.add(rstMeta.getColumnName(c));
+				rstMeta.getColumnType(c);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return colNames;
+	}
+
+	// Get type of data in a column for a query result set.
+	public static Vector<Integer> getColumnTypes(ResultSet rst) {
+		Vector<Integer> colTypes = new Vector<Integer>();
+
+		// TODO: Figure out a better way without potentially retrieving the whole table.
+		try {
+
+			ResultSetMetaData rstMeta = rst.getMetaData();
+
+			int numCols = rstMeta.getColumnCount();
+
+			for (int c = 1; c <= numCols; ++c) {
+				int t = rstMeta.getColumnType(c); // TODO: Why does it returns an "int" when doc says "java.sql.Types".
+				colTypes.add(t);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return colTypes;
+	}
+
+	// All possible main menu options.
 	enum MainMenuOption {
-		ADD,
-		UPDATE,
-		DELETE,
-		SEARCH,
-		REPORT,
-		EXIT
+		ADD, UPDATE, DELETE, SEARCH, REPORT, EXIT
 	};
-	
+
 	// Prompt user with a main menu selection.
 	public static MainMenuOption promptMainMenu() {
-				
+
 		// Prompt user with a main menu
 		boolean isValid = false;
 		int choice = 0;
-		while(!isValid) {
+		while (!isValid) {
 			// Show main menu
 			System.out.println("-- Main Menu --");
-			System.out.println(
-					"Select an option: (int)\n" +
-					"	1) Add Record(s)\n" +
-					"	2) Update Record(s)\n" +	
-					"	3) Delete Record(s)\n" +
-					"	4) Search Record(s)\n" +
-					"   	*) Exit\n"
-			);
-			
+			System.out.println("Select an option: (int)\n" + "	1) Add Record(s)\n" + "	2) Update Record(s)\n"
+					+ "	3) Delete Record(s)\n" + "	4) Search Record(s)\n" + "   	*) Exit\n");
+
 			// Read integer input.
 			try {
 				choice = in.nextInt();
 				isValid = true;
-			}
-			catch (java.util.InputMismatchException ie) {
+			} catch (java.util.InputMismatchException ie) {
 				// restart if not a valid integer.
 				System.out.println("X Please enter an integer! X");
 				in.next(); // Read any pending input
 			}
-			
+
 		}
-		
+
 		switch (choice) {
-		
+
 		// Add record
 		case 1:
 			return MainMenuOption.ADD;
-			//break;
-		
+		// break;
+
 		// Update record
 		case 2:
 			return MainMenuOption.UPDATE;
-			//break;
-		
+		// break;
+
 		case 3:
 			return MainMenuOption.DELETE;
-			//break;
-		
+		// break;
+
 		case 4:
 			return MainMenuOption.SEARCH;
-			//break;
-			
+		// break;
+
 		default:
 			return MainMenuOption.EXIT;
-			//break;
+		// break;
 		}
-		
+
 	}
 
 	// Prompt user to get the name of table to modify and check for its validity
-public static String promptTableSelection(Connection conn) {
-		
+	public static String promptTableSelection(Connection conn) {
+
 		System.out.println("Which table do you want to address?: (string)");
-		
+
 		try {
 			String tName = in.next();
 			in.nextLine(); // Consume rest of the line
-			
-	    	// Make sure table name provided exists in the database.
-			HashSet<String> tableNames = getTableNames(conn);
-	    	if (!tableNames.contains(tName)) {
-	    		
-	    		System.out.println("X Given table name does not exist in the database. X"); // TODO: Better error handling.
-	    		System.out.println("Please select from: ");
-	    		tableNames.forEach( element -> {
-	    		    System.out.println(element);
-	    		});
-	    		
-	    		//TODO: Handle this part better.
-	    		System.exit(1);
-	    	}
 
-	    	return tName;
-		}
-		catch (Exception e) {
+			// Make sure table name provided exists in the database.
+			HashSet<String> tableNames = getTableNames(conn);
+			if (!tableNames.contains(tName)) {
+
+				System.out.println("X Given table name does not exist in the database. X"); // TODO: Better error
+																							// handling.
+				System.out.println("Please select from: ");
+				tableNames.forEach(element -> {
+					System.out.println(element);
+				});
+
+				// TODO: Handle this part better.
+				System.exit(1);
+			}
+
+			return tName;
+		} catch (Exception e) {
 			System.out.println("X Error! X");
 			throw e; // TODO: Better exception handling.
 		}
-		
+
 	}
-	
-	
+
 	// Collect input from user for the records to add.
 	public static Map<Integer, String> promptAddRecords() {
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.print("How many records would you like to add?: (int)\n");
-		int numRecords = in.nextInt();	
-		
+		int numRecords = in.nextInt();
+
 		System.out.print("** Enter data for each record followed by enter key. **\n");
-		
+
 		Map<Integer, String> recordStrings = new HashMap<Integer, String>();
-		
-		for(int i = 0; i <= numRecords; i++) {
+
+		for (int i = 0; i <= numRecords; i++) {
 			String data = in.nextLine();
 			recordStrings.put(i, data);
 		}
 		recordStrings.remove(0);
 
 		in.close();
-		
+
 		// Print data for debug if needed
 //		logger.fine(recordStrings.toString());
-		
-		
+
 		return recordStrings;
 	}
-	
-	
+
 	public static void updateRecords(Connection conn) {
 		Scanner in = new Scanner(System.in);
-		
-		
+
 //		sArray.addAll(s); //transfer set data to arrayList
 //		System.out.println(sArray);
 //		System.out.print("Do you want to update data? (Y/N): ");
@@ -317,39 +302,37 @@ public static String promptTableSelection(Connection conn) {
 //		// Print data for debug if needed
 //		logger.fine(recordStrings.toString());;
 	}
-		public static String promptDeleteRecords() {
-		
-			System.out.print("Please enter the condition for selecting records to delete.\n");
-			String delCondition = in.nextLine();
-		
-			return delCondition;
+
+	public static String promptDeleteRecords() {
+
+		System.out.print("Please enter the condition for selecting records to delete.\n");
+		String delCondition = in.nextLine();
+
+		return delCondition;
+	}
+
+	public static int deleteRecords(Connection conn, String tableName, String delCondition) {
+
+		int numRecordsAffected = 0;
+		try {
+			String prepQuery = "DELETE FROM " + tableName + "\n" + "WHERE " + delCondition + " ;";
+
+			PreparedStatement pStmt = conn.prepareStatement(prepQuery);
+
+			numRecordsAffected = pStmt.executeUpdate();
+
+			System.out.println("Number of records deleted: " + numRecordsAffected);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.exit(1); // TODO: Better error handling
 		}
-	
-		public static int deleteRecords(Connection conn, String tableName, String delCondition) { 
-	    	
-			int numRecordsAffected = 0;
-	    	try {
-		    	String prepQuery = "DELETE FROM " + tableName + "\n" +
-						   		   "WHERE " + delCondition + " ;";
-		    	
-		    	PreparedStatement pStmt=  conn.prepareStatement(prepQuery);
-		    		    	
-		    	numRecordsAffected = pStmt.executeUpdate();
-		    	
-		    	System.out.println("Number of records deleted: " + numRecordsAffected);
-		    		    	
-	    	}
-	    	catch (Exception e) {
-	    		System.out.println(e.getMessage());
-	    		System.exit(1); // TODO: Better error handling
-	    	}
-	    	
-	    	return numRecordsAffected;
-				
-		}
-	
-	
-	public static void searchRecords() { 
+
+		return numRecordsAffected;
+
+	}
+
+	public static void searchRecords() {
 //		System.out.print("Search the data? (Y/N): ");
 //		option = in.nextLine();
 //		while (!option.equals("N")) {
@@ -362,82 +345,75 @@ public static String promptTableSelection(Connection conn) {
 //		}
 //	
 	}
-	
-	
+
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
 		Connection conn = initializeDB("data/YUZU.db");
 		PreparedStatement stmt = null;
 		ResultSet rSet = null;
-		
-		
-		try {
-			
-		MainMenuOption selectedOption = promptMainMenu();
-		
-		switch (selectedOption) {
-		
-		case ADD:
-			Scanner userInput = new Scanner(System.in);
-			String tName = promptTableSelection(conn);
-			String sql = "SELECT * FROM " + tName + ";"; //Creates builder query in order to access metadata
-			stmt = conn.prepareStatement(sql);
-			rSet = stmt.executeQuery(); //Query executes stmt 		
-			
-			for(int i = 0; i < rSet.getMetaData().getColumnCount(); i++) {
-			sb.append(rSet.getMetaData().getColumnName(i+1) + ",");
-			}
-			
-			sb.deleteCharAt(sb.length() - 1); //Removes comma on end of stringbuilder
-			String cNames = sb.toString();
-			sb = new StringBuilder();
-			Map<Integer, String> recordsToAdd = promptAddRecords();
 
-			PreparedStatement stmt2 = null;
-			ResultSet rSet2 = null;
-			for(int key: recordsToAdd.keySet()) {
-				String value = recordsToAdd.get(key);
-				System.out.println(value);
-				String sqlStatement = "INSERT INTO " + tName + "(" + cNames + ") " + "VALUES(" + value + ");";
-		 		stmt2 = conn.prepareStatement(sqlStatement);
-		 		
-				rSet2 = stmt2.executeQuery();
-		 		
+		try {
+
+			MainMenuOption selectedOption = promptMainMenu();
+
+			switch (selectedOption) {
+
+			case ADD:
+				Scanner userInput = new Scanner(System.in);
+				String tName = promptTableSelection(conn);
+				String sql = "SELECT * FROM " + tName + ";"; // Creates builder query in order to access metadata
+				stmt = conn.prepareStatement(sql);
+				rSet = stmt.executeQuery(); // Query executes stmt
+
+				for (int i = 0; i < rSet.getMetaData().getColumnCount(); i++) {
+					sb.append(rSet.getMetaData().getColumnName(i + 1) + ",");
+				}
+
+				sb.deleteCharAt(sb.length() - 1); // Removes comma on end of stringbuilder
+				String cNames = sb.toString();
+				sb = new StringBuilder();
+				Map<Integer, String> recordsToAdd = promptAddRecords();
+
+				PreparedStatement stmt2 = null;
+				ResultSet rSet2 = null;
+				for (int key : recordsToAdd.keySet()) {
+					String value = recordsToAdd.get(key);
+					System.out.println(value);
+					String sqlStatement = "INSERT INTO " + tName + "(" + cNames + ") " + "VALUES(" + value + ");";
+					stmt2 = conn.prepareStatement(sqlStatement);
+
+					rSet2 = stmt2.executeQuery();
+
+				}
+
+				System.out.println("ADD was SUCCESSFUL");
+
+				// Test Inputs:
+				// 100099,Andrew,J,Kelly,22,Male,abcde,fghij,513-687-5746,kelly15@yahoo.com
+				// 100100,Mason,K,Dempsy,19,Male,qwerty,asdfgh,513-250-2692,dempsy@gmail.com
+				// 100101,Jacob,B,Stuart,20,Male,cuandxl,aiawkgh,614-398-2247,stuuy@outlook.com
+
+				break;
+			case UPDATE:
+
+				break;
+			case DELETE: {
+				String tableName = promptTableSelection(conn);
+				String delCondition = promptDeleteRecords();
+				deleteRecords(conn, tableName, delCondition);
+				break;
 			}
-	 		
-	 		
-	 		System.out.println("ADD was SUCCESSFUL");
-	 			
-	 			//Test Inputs:
-	 		//100099,Andrew,J,Kelly,22,Male,abcde,fghij,513-687-5746,kelly15@yahoo.com
-	 		//100100,Mason,K,Dempsy,19,Male,qwerty,asdfgh,513-250-2692,dempsy@gmail.com
-	 		//100101,Jacob,B,Stuart,20,Male,cuandxl,aiawkgh,614-398-2247,stuuy@outlook.com
-	 		
-			
-			
-			break;
-		case UPDATE:
-			
-			break;
-		case DELETE: {
-			String tableName = promptTableSelection(conn);
-			String delCondition = promptDeleteRecords();
-			deleteRecords(conn, tableName, delCondition);
-			break;
-		}
-		case SEARCH:
-			break;
-		case REPORT:
-			break;
-		case EXIT:
-			break;
-		}
-		
-			
-		} catch (Exception e){
+			case SEARCH:
+				break;
+			case REPORT:
+				break;
+			case EXIT:
+				break;
+			}
+
+		} catch (Exception e) {
 			System.out.println("ERROR");
 		}
-	
-		
+
 	}
 }
